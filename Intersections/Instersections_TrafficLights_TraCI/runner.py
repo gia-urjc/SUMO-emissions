ace161cf6c2478d24e7cd576479961b0934051ba
sandruskyi <sandragomezgalvez@hotmail.com>
2020-12-04 00:52:21 +0100

    Runner creates the route

--- Intersections/Instersections_TrafficLights_TraCI/runner.py
+++ Intersections/Instersections_TrafficLights_TraCI/runner.py
@@ -8,10 +8,12 @@ if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
 else:
-    sys.exit("please declare environment variable 'SUMO_HOME' ")
+    sys.exist("please declare environment variable 'SUMO_HOME'")
 
-from sumolib import checkBinary
-import traci
+
+from sumolib import checkBinary  # noqa
+import traci  # noqa
+
 
 def generateRoutes():
     random.seed(1)
@@ -20,11 +22,13 @@ def generateRoutes():
     probabilitySN = 1./ 12  # Del Sur al Norte
     probabilityEO = 1. / 8
     probabilityOE = 1. / 9
-    probabilityAutonomous = 1./3
+    pAutonomousNS = 1./15
+    pAutonomousSN = 2./15
+    pAutonomousEO = 1. / 15
+    pAutonomousOE = 2. / 15
 
     with open("intersec_TL.rou.xml", "w") as routes:
-        original_stdout = sys.stdout
-        sys.stdout = routes
+
         print(""" <routes>
         <vType id="autonomous_car" vClass="evehicle" color="green"/>
         <vType id="normal_car" color="255,0,170"/>
@@ -32,35 +36,46 @@ def generateRoutes():
         <route edges="-e4 e2" color="yellow" id="route_1"/>
         <route edges="-e3 -e1" color="yellow" id="route_2"/>
         <route edges="-e2 e4" color="yellow" id="route_3"/>
-        <route edges="e1 e3" color="yellow" id="route_4"/>""") #OE SN EO NS
+        <route edges="e1 e3" color="yellow" id="route_4"/>""", file = routes) #OE SN EO NS
 
         vehNr = 0
 
         for i in range(timeStep):
             if random.uniform(0,1)<probabilityNS:
                 print('     <vehicle id="NS%i" type="normal_car" route="route_4" depart="%i" />' % (
-                        vehNr, i))
+                        vehNr, i), file =routes)
                 vehNr +=1
             if random.uniform(0, 1) < probabilitySN:
                 print('     <vehicle id="SN%i" type="normal_car" route="route_2" depart="%i" />' % (
-                    vehNr, i))
+                    vehNr, i), file=routes)
                 vehNr += 1
             if random.uniform(0, 1) < probabilityEO:
                 print('     <vehicle id="EO%i" type="normal_car" route="route_3" depart="%i" />' % (
-                    vehNr, i))
+                    vehNr, i), file=routes)
                 vehNr += 1
             if random.uniform(0, 1) < probabilityOE:
                 print('     <vehicle id="NS%i" type="normal_car" route="route_1" depart="%i" />' % (
-                    vehNr, i))
+                    vehNr, i), file=routes)
                 vehNr += 1
             possible_routes = ["route_1","route_2","route_3","route_4"]
 
-            if random.uniform(0, 1) < probabilityAutonomous:
-                print('     <vehicle id="Au{}" type="autonomous_car" route="{}" depart="{}"').format(
-                    vehNr,random.choice(possible_routes),i)
+            if random.uniform(0, 1) < pAutonomousNS:
+                print('    <vehicle id="Au%i" type="autonomous_car" route="route_4" depart="%i" />' % (
+                    vehNr, i), file=routes)
                 vehNr += 1
-        print("</routes>")
-        sys.stdout = original_stdout
+            if random.uniform(0, 1) < pAutonomousSN:
+                print('    <vehicle id="Au%i" type="autonomous_car" route="route_2" depart="%i" />' % (
+                    vehNr, i), file=routes)
+                vehNr += 1
+            if random.uniform(0, 1) < pAutonomousEO:
+                print('    <vehicle id="Au%i" type="autonomous_car" route="route_3" depart="%i" />' % (
+                    vehNr, i), file=routes)
+                vehNr += 1
+            if random.uniform(0, 1) < pAutonomousOE:
+                print('    <vehicle id="Au%i" type="autonomous_car" route="route_1" depart="%i" />' % (
+                    vehNr, i), file=routes)
+                vehNr += 1
+        print("</routes>", file=routes)
 
 
 
@@ -83,14 +98,14 @@ def get_options():
 
 if __name__ == "__main__":
     options = get_options()
-
+    print("HOLA")
     if options.nogui:
         sumoBinary = checkBinary("SUMO")
     else:
         sumoBinary = checkBinary("sumo-gui")
-
+    print("NO")
     generateRoutes()
-
-    traci.start([sumoBinary, "-c", "cross.sumocfg", "--tripinfo-output", "tripinfo.xml"])
+    print("SI")
+    traci.start([sumoBinary, "-c", "intersec_TL.sumocfg", "--tripinfo-output", "tripinfo.xml"])
 
     run()
