0e407ef94935b0465bef8abfb92e4f20f53e3884
sandruskyi <sandragomezgalvez@hotmail.com>
2020-12-03 18:11:00 +0100

    Runner with several errors | TrafficLights_TraCI

--- Intersections/Instersections_TrafficLights_TraCI/runner.py
+++ Intersections/Instersections_TrafficLights_TraCI/runner.py
@@ -13,10 +13,65 @@ else:
 from sumolib import checkBinary
 import traci
 
+def generateRoutes():
+    random.seed(1)
+    timeStep = 300
+    probabilityNS = 1./10 # Del Norte al Sur
+    probabilitySN = 1./ 12  # Del Sur al Norte
+    probabilityEO = 1. / 8
+    probabilityOE = 1. / 9
+    probabilityAutonomous = 1./3
+
+    with open("intersec_TL.rou.xml", "w") as routes:
+        original_stdout = sys.stdout
+        sys.stdout = routes
+        print(""" <routes>
+        <vType id="autonomous_car" vClass="evehicle" color="green"/>
+        <vType id="normal_car" color="255,0,170"/>
+        
+        <route edges="-e4 e2" color="yellow" id="route_1"/>
+        <route edges="-e3 -e1" color="yellow" id="route_2"/>
+        <route edges="-e2 e4" color="yellow" id="route_3"/>
+        <route edges="e1 e3" color="yellow" id="route_4"/>""") #OE SN EO NS
+
+        vehNr = 0
+
+        for i in range(timeStep):
+            if random.uniform(0,1)<probabilityNS:
+                print('     <vehicle id="NS%i" type="normal_car" route="route_4" depart="%i" />' % (
+                        vehNr, i))
+                vehNr +=1
+            if random.uniform(0, 1) < probabilitySN:
+                print('     <vehicle id="SN%i" type="normal_car" route="route_2" depart="%i" />' % (
+                    vehNr, i))
+                vehNr += 1
+            if random.uniform(0, 1) < probabilityEO:
+                print('     <vehicle id="EO%i" type="normal_car" route="route_3" depart="%i" />' % (
+                    vehNr, i))
+                vehNr += 1
+            if random.uniform(0, 1) < probabilityOE:
+                print('     <vehicle id="NS%i" type="normal_car" route="route_1" depart="%i" />' % (
+                    vehNr, i))
+                vehNr += 1
+            possible_routes = ["route_1","route_2","route_3","route_4"]
+
+            if random.uniform(0, 1) < probabilityAutonomous:
+                print('     <vehicle id="Au{}" type="autonomous_car" route="{}" depart="{}"').format(
+                    vehNr,random.choice(possible_routes),i)
+                vehNr += 1
+        print("</routes>")
+        sys.stdout = original_stdout
+
+
+
+
+
+
+
 def run():
 
     while traci.simulation.getMinExpectedNumber() > 0: # While there are cars (and waiting cars)
-        traci.simulationStep() # Advance one time
+        traci.simulationStep() # Advance one time step
 
     traci.close()
     sys.stdout.flush()
@@ -33,6 +88,8 @@ if __name__ == "__main__":
         sumoBinary = checkBinary("SUMO")
     else:
         sumoBinary = checkBinary("sumo-gui")
+
+    generateRoutes()
 
     traci.start([sumoBinary, "-c", "cross.sumocfg", "--tripinfo-output", "tripinfo.xml"])
 
