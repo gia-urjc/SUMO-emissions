a807758ce7acffa03af8889df5952b7bb0db622e
sandruskyi <sandragomezgalvez@hotmail.com>
2021-02-03 16:03:34 +0100

    Create main.py

---
+++ Priorizite_Access_Emissions/case3 - OOP/main.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import optparse
+import random
+
+# import python modules from $SUMO/HOME directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exist("please declare environment variable 'SUMO_HOME'")
+
+from sumolib import checkBinary  # noqa
+import traci  # noqa
+
+from Vehicle import Vehicle
+from Simulation import Simulation
+from Window import Window
