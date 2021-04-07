a5f49be5f0e93bbea51cba4da3b52c5ef2f28fc3
sandruskyi <sandragomezgalvez@hotmail.com>
2021-04-07 14:03:59 +0200

    Fix results.py

--- generate_historical/generateHistorical.py
+++ generate_historical/generateHistorical.py
@@ -1,9 +1,10 @@
 from SUMO_emissions_runner.main import run_main
 from configuration import readConfigurationCSV as rCSV
 import pandas as pd
+import os
 
-if __name__ == "__main__":
 #def generateHistorical():
+if __name__ == "__main__":
     strategy, timeStep, probability_E, probability_G, probability_D, probability_HG, probability_N, probability_H, probability_T, \
     window_size, threshold_L, threshold_H, p_t_ini, size_ratio, subs_NOx, e_ini, \
     min_packages, max_packages, control_area_edges_cnf, enter_control_area_edges = \
@@ -15,17 +16,21 @@ if __name__ == "__main__":
 
     if election == "1":
         print("1")
-        route = r"./noControl/results_file_noControl.csv" # CHANGE IF YOU NEED
+        route = r"../generate_historical/noControl/results_file_noControl.csv" # CHANGE IF YOU NEED
     elif election == "2":
         print("First, runs noControl to create the historical")
-        route = r"./noControl/results_file_noControl.csv" # CHANGE IF YOU NEED
+        if not os.path.exists("noControl"): # If the folder doesn't exists  -> Create folder
+            os.makedirs("noControl")
+        route = r"../generate_historical/noControl/results_file_noControl.csv" # CHANGE IF YOU NEED
         # TODO: Crear una condicion para que no se reescriba un nocontrol ya creado
         run_main("noControl", "", dict(), window_size, threshold_L, threshold_H, p_t_ini, size_ratio,
                    subs_NOx, e_ini, min_packages, max_packages, control_area_edges_cnf, enter_control_area_edges, route)
 
     print("Creating historical...")
 
-    df = pd.read_csv(route, delimiter=",")
+    route = r"../generate_historical/noControl/results_file_noControl.csv"  # CHANGE IF YOU NEED
+    df = pd.read_csv(route, delimiter=";", header=None)
+    print("HOLA")
     df.head()
 
     # Con el archivo No control obtener NOx_total y total_time
