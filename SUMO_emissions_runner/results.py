5433172b6649ff9fb558ec4f9c17d60151b1961a
sandruskyi <sandragomezgalvez@hotmail.com>
2021-04-07 15:03:10 +0200

    Generate_historical using results & no control

--- SUMO_emissions_runner/results.py
+++ SUMO_emissions_runner/results.py
@@ -1,6 +1,5 @@
 """ Write results in a file """
 from pathlib import Path
-import sys
 import os
 import csv
 
@@ -23,6 +22,8 @@ def results(simulation, window_size, p_t_ini, size_ratio, subs_NOx, e_ini, min_p
             fileName = r"./results/" + file + str(cont_file) + ".csv"
             print(fileName)
             fileObject = Path(fileName)
+
+
     else:
         fileName = route
 
@@ -30,10 +31,12 @@ def results(simulation, window_size, p_t_ini, size_ratio, subs_NOx, e_ini, min_p
         print(fileName)
         f = csv.writer(f_csv, delimiter=';')
         """Second, we write the parameters"""
+        f.writerow(["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"])
         f.writerow([simulation.strategy])
         f.writerow(["PARAMETERS"])
 
-        f.writerow(["window_size", "threshold_L", "threshold_H", "p_t_ini", "e_ini", "size_ratio", "subs_NOx", "min_packages", "max_packages"])
+        f.writerow(["window_size", "threshold_L", "threshold_H", "p_t_ini", "e_ini", "size_ratio", "subs_NOx",
+                    "min_packages", "max_packages"])
         f.writerow([str(window_size), str(simulation.threshold_L), str(simulation.threshold_H), str(p_t_ini),
                     str(e_ini), str(size_ratio), str(subs_NOx), str(min_packages), str(max_packages)])
 
@@ -51,7 +54,7 @@ def results(simulation, window_size, p_t_ini, size_ratio, subs_NOx, e_ini, min_p
             avg_k_all_steps += w.k
             acum += w.NOx_total_w
 
-        avg_k_all_steps = avg_k_all_steps/len(simulation.windows)
+        avg_k_all_steps = avg_k_all_steps / len(simulation.windows)
         # Vehicles values
         p_all, cont, avg_contrib, total_packages, enter_cz_all_steps, not_enter_cz_all_steps, avg_total_time_all_steps = 0, 0, 0, 0, 0, 0, 0
         for v in simulation.all_veh:
@@ -64,7 +67,7 @@ def results(simulation, window_size, p_t_ini, size_ratio, subs_NOx, e_ini, min_p
             if v.enter_cz == True:
                 enter_cz_all_steps += 1
             else:
-                not_enter_cz_all_steps +=1
+                not_enter_cz_all_steps += 1
 
         avg_total_time_all_steps = avg_total_time_all_steps / cont
         avg_time_per_package = avg_contrib / total_packages
@@ -73,8 +76,9 @@ def results(simulation, window_size, p_t_ini, size_ratio, subs_NOx, e_ini, min_p
 
         f.writerow(["ALL SIMULATION RESULTS"])
         f.writerow(["total_steps(sec)", "minutes", "avg_time_per_package (sec)", "p_t_total_all_steps",
-                    "e_t_total_all_steps", "p_t_control_zone_all_steps",  "e_t_control_zone_all_steps",
-                    "avg_k_all_steps", "veh_enter_cz_all_steps", "not_veh_enter_cz_all_steps", "avg_total_time_all_steps"])
+                    "e_t_total_all_steps", "p_t_control_zone_all_steps", "e_t_control_zone_all_steps",
+                    "avg_k_all_steps", "veh_enter_cz_all_steps", "not_veh_enter_cz_all_steps",
+                    "avg_total_time_all_steps"])
         f.writerow([str(simulation.step), str(minutes), str(avg_time_per_package), str(p_t_total_all_steps),
                     str(e_t_total_all_steps), str(p_t_control_zone_all_steps), str(e_t_control_zone_all_steps),
                     str(avg_k_all_steps), str(enter_cz_all_steps), str(not_enter_cz_all_steps),
@@ -90,9 +94,10 @@ def results(simulation, window_size, p_t_ini, size_ratio, subs_NOx, e_ini, min_p
         for w in simulation.windows:
             acum += w.NOx_total_w
             f.writerow([str(w.step), str(w.NOx_total_w), str(acum), str(w.lambda_l), str(w.p_t_total),
-                        str(w.NOx_total_w), str(w.p_t), str(w.NOx_control_zone_w), str(w.k), str(w.veh_total_number_w),
+                        str(w.NOx_total_w), str(w.p_t), str(w.NOx_control_zone_w), str(w.k),
+                        str(w.veh_total_number_w),
                         str(len(w.vehicles_in_control_zone_w))])
-        #avg_k_all_steps = avg_k_all_steps / cont
+        # avg_k_all_steps = avg_k_all_steps / cont
 
         f.writerow(["VEHICLES"])
         f.writerow(["id", "vType", "NOx_total_veh", "n_packages", "step_ini", "step_fin", "total_time(sec)",
@@ -105,10 +110,38 @@ def results(simulation, window_size, p_t_ini, size_ratio, subs_NOx, e_ini, min_p
 
         enter_cz_all_steps = 0
 
+        vTypes_vehs = set() # For historical
+        NOx_total_vehs = dict() # For historical
+        total_time_vehs = dict() # For historical
+        count_vehs = dict()  # For historical
+
         for v in simulation.all_veh:
             total_time = v.step_fin - v.step_ini
             average_package = total_time / v.n_packages
             if v.enter_cz == True: enter_cz_all_steps += 1
-            f.writerow([v.id, v.vType, str(v.NOx), str(v.n_packages), str(v.step_ini), str(v.step_fin), str(total_time),
-                        str(average_package), str(v.enter_cz)])
+            f.writerow(
+                [v.id, v.vType, str(v.NOx), str(v.n_packages), str(v.step_ini), str(v.step_fin), str(total_time),
+                 str(average_package), str(v.enter_cz)])
+
+            # For historical :
+            vTypes_vehs.add(v.vType)
+            if not v.vType in NOx_total_vehs:
+                NOx_total_vehs[v.vType] = 0
+                total_time_vehs[v.vType] = 0
+                count_vehs[v.vType] = 0
+            NOx_total_vehs[v.vType] += v.NOx
+            total_time_vehs[v.vType] += total_time
+            count_vehs[v.vType] += 1
+
+
+
+        # FOR HISTORICAL
+        f.writerow(["FOR_HISTORICAL"])
+        f.writerow(["vType", "NOx_total", "total_time"])
+
+        for v in vTypes_vehs:
+            NOx_write = NOx_total_vehs[v] / count_vehs[v]
+            total_time_write = total_time_vehs[v] / count_vehs[v]
+            f.writerow([v, NOx_write, total_time_write])
+
 
--- generate_historical/generateHistorical.py
+++ generate_historical/generateHistorical.py
@@ -16,22 +16,22 @@ if __name__ == "__main__":
 
     if election == "1":
         print("1")
-        route = r"../generate_historical/noControl/results_file_noControl.csv" # CHANGE IF YOU NEED
+        route = r"../generate_historical/noControl_resultsHistorical/results_file_noControl.csv" # CHANGE IF YOU NEED
     elif election == "2":
         print("First, runs noControl to create the historical")
-        if not os.path.exists("noControl"): # If the folder doesn't exists  -> Create folder
-            os.makedirs("noControl")
-        route = r"../generate_historical/noControl/results_file_noControl.csv" # CHANGE IF YOU NEED
+        if not os.path.exists("noControl_resultsHistorical"): # If the folder doesn't exists  -> Create folder
+            os.makedirs("noControl_resultsHistorical")
+        route = r"../generate_historical/noControl_resultsHistorical/results_file_noControl.csv" # CHANGE IF YOU NEED
         # TODO: Crear una condicion para que no se reescriba un nocontrol ya creado
         run_main("noControl", "", dict(), window_size, threshold_L, threshold_H, p_t_ini, size_ratio,
                    subs_NOx, e_ini, min_packages, max_packages, control_area_edges_cnf, enter_control_area_edges, route)
 
     print("Creating historical...")
-
-    route = r"../generate_historical/noControl/results_file_noControl.csv"  # CHANGE IF YOU NEED
+#if __name__ == "__main__":
+    route = r"../generate_historical/noControl_resultsHistorical/results_file_noControl.csv"  # CHANGE IF YOU NEED
     df = pd.read_csv(route, delimiter=";", header=None)
     print("HOLA")
-    df.head()
+    print(df.head())
 
     # Con el archivo No control obtener NOx_total y total_time
     # En el archivo sumo_emissions_runner/createHistorical está como leer el XML de rutas generadas que lo necesitaremos
