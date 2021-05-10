bc104fbebab6f72b816752924ae0c47e6ed7e401
sandruskyi <sandragomezgalvez@hotmail.com>
2021-05-10 12:04:29 +0200

    timeStep to number_of_time_steps

--- calculate_density_distribution/calculateDensityDistribution.py
+++ calculate_density_distribution/calculateDensityDistribution.py
@@ -57,7 +57,7 @@ def calculateDensityDistribution():
     ##################################
     ##################################
 
-    strategy, timeStep, probability_E, probability_G, probability_D, probability_HD, probability_N, probability_H, probability_T, \
+    strategy, number_of_time_steps, probability_E, probability_G, probability_D, probability_HD, probability_N, probability_H, probability_T, \
     window_size, threshold_L, threshold_H, p_t_ini, size_ratio, subs_NOx, e_ini, \
     ini_lambda_l, min_randomLambda, max_randomLambda, ini_k_window, \
     min_packages, max_packages, control_area_edges_cnf, enter_control_area_edges = \
